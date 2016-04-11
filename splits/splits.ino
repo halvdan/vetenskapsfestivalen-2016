f83edcb9025bf1e3e7a3b3982b1803a46668e551
Dan Johansson <dan.albin.johansson@gmail.com>
2016-04-11 21:47:26 +0200

    Make use of Bounce2

--- splits/splits.ino
+++ splits/splits.ino
@@ -1,167 +1,76 @@
+#include <Bounce2.h>
+
 #define LED 13
 #define ARDUINO_SIGNAL_OUT 8
 #define ARDUINO_SIGNAL_IN 7
-#define SENSOR_IN 0
-
-#define SIGNAL_PIN 7
-
-enum split_state {
-  READY,
-  START,
-  RUNNING,
-  DONE
-};
-
-split_state current_state = RUNNING;
+#define SENSOR_IN 2
 
 int sensor;
 int arduino_signal;
 
+unsigned long split_time = 0;
 unsigned long start_time = 0;
-unsigned long foo_time = 0;
-unsigned long bar_time = 0;
 
-float previous_time = 0;
 float best_time = 0;
 
+
+float previous_time = 0;
+
 unsigned long ms = 0;
+
+Bounce debouncer = Bounce();
 
 void setup() {
   pinMode(LED, OUTPUT);
   pinMode(ARDUINO_SIGNAL_OUT, OUTPUT);
   pinMode(ARDUINO_SIGNAL_IN, INPUT);
   pinMode(SENSOR_IN, INPUT);
+
+  debouncer.attach(SENSOR_IN);
+  debouncer.interval(5);
+  
+  digitalWrite(ARDUINO_SIGNAL_OUT, LOW);
   
   Serial.begin(9600);
-
-  foo_done();
 }
 
 void loop() {
-  sensor = digitalRead(SENSOR_IN);
+  debouncer.update();
+
+  int sensor = debouncer.read();
+
   arduino_signal = digitalRead(ARDUINO_SIGNAL_IN);
 
-  Serial.println(sensor);
-
+  // Fix sensor trigger
   if (sensor == LOW) {
-    if (current_state == RUNNING) {
-      //sensor_activated();
+    split_time = millis();
+    
+    digitalWrite(LED, HIGH);
+    digitalWrite(ARDUINO_SIGNAL_OUT, HIGH);
+    delay(10);
+    digitalWrite(ARDUINO_SIGNAL_OUT, LOW);
 
-      analogWrite(LED, 255);
-      delay(1000);
+    if (start_time > 0) {
+      float final_time = calc_time(start_time, split_time);
+      Serial.println("-----");
+      Serial.print(final_time, 3);
+      Serial.println(" s");
+      Serial.println("-----");
+
+      if (best_time == 0 || final_time < best_time) {
+        best_time = final_time;
+        Serial.println("!!! NEW RECORD !!!");
+        // SAVE TO EEPROM
+      }
     }
   }
 
-  delay(1000);
-
-/*
-
-  if (arduino_signal == LOW) {
-    switch (arduino_signal) {
-      case READY:
-        foo_ready();
-        break;
-      case START:
-        foo_start();
-        break;
-      case RUNNING:
-        foo_running();
-        break;
-      case DONE:
-        foo_done();
-        break;
-    }
+  if (arduino_signal == HIGH) {
+    Serial.println("ZING!!!!!!!");
+    start_time = millis();
+    digitalWrite(LED, LOW);
   }
-  */
 }
-
-/**
- * Received signal from while in state ready. This means that the 
- * lap has started and thus we start the timer. And update our 
- * state. And notify the next controller in the chain.
- */
-void foo_ready() {
-  start_time = millis();
-  current_state = START;
-  digitalWrite(ARDUINO_SIGNAL_OUT, LOW);
-  delay(10);
-  digitalWrite(ARDUINO_SIGNAL_OUT, HIGH);
-}
-
-/**
- * Received signal while in state START.
- * 
- * The runner has reached the previous sensor and is en route to 
- * this sensor. Therefore we note the time since start. Update 
- * state to RUNNING.
- */
-void foo_start() {
-  foo_time = millis();
-  current_state = RUNNING;
-}
-
-/**
- * Received signal while in state RUNNING.
- */
-void foo_running() {
-  // DO NOTHING
-}
-
-/**
- * Received signal while in state DONE.
- * 
- * Indicating that we have come full circle and are wrapping 
- * up stuff and are preparing for a new race. Update state 
- * to READY.
- */
-void foo_done() {
-  current_state = READY;
-  float final_time = calc_time(start_time, millis());
-
-  Serial.println("-----");
-  Serial.print(final_time, 3);
-  Serial.println(" s");
-  Serial.println("-----");
-}
-
-/**
- * Received signal from our sensor.
- * 
- * Update time, notify next Arduino. Update state.
- */
-void sensor_activated() {
-  bar_time = millis();
-  current_state = DONE;
-  digitalWrite(ARDUINO_SIGNAL_OUT, LOW);
-  delay(10);
-  digitalWrite(ARDUINO_SIGNAL_OUT, HIGH);
-}
-/*
-void start_stuff() {
-  ms = millis();
-  is_running = true;
-  analogWrite(RED, 0);
-  analogWrite(GRN, 255);
-  delay(1000);
-}
-
-void stop_stuff() {
-  float current_time = calc_time(ms, millis());
-  
-  if (current_time < best_time) {
-    best_time = current_time;
-  }
-
-  previous_time = current_time;
-  is_running = false;
-
-  Serial.print(current_time, 3);
-  Serial.println("s");
-  
-  analogWrite(GRN, 0);
-  analogWrite(RED, 255);
-  delay(1000);
-}*/
 
 float calc_time(long start_millis, long stop_millis) {
   long ms = stop_millis - start_millis;
